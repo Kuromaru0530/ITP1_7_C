7b29101d8177ad926b7b947a3579821a700a17e1
Kuromaru0530 <122763982+Kuromaru0530@users.noreply.github.com>
2023-04-10 16:16:44 +0900

    Update Program.cs

--- ITP1_7_C/Program.cs
+++ ITP1_7_C/Program.cs
@@ -31,51 +31,6 @@ namespace ITP1_7_C
                 }
                 Console.WriteLine();
             }
-            
-            //int Count_1 = 0;
-            //int Count_2 = 0;
-
-            //for (int i = 0; i < r; i++)
-            //{              
-            //    for (int j = 0; j < c; j++)
-            //    {  
-            //        Count_1 += Array[i, j];
-            //    }
-            //    Array[i, c + 1] = Count_1;
-            //}
-
-            //for(int k = 0; k < c; k++)
-            //{               
-            //    for (int l = 0; l < r; l++)
-            //    {   
-            //        Count_2 += Array[l, k];    
-            //    }
-            //    Array[r + 1, k] = Count_2;
-            //}
-
-            //int Sum1 = 0;
-            //int Sum2 = 0;
-            //for (int m = 0; m < c; m++)
-            //{
-            //    Sum1 += Array[r + 1, m];
-            //}
-
-            //for (int o = 0; o < r; o++)
-            //{
-            //    Sum2 += Array[o, c + 1];
-            //}
-            //Array[r + 1, c + 1] = Sum1 + Sum2;
-
-            //for(int p = 0; p < r + 1; p++)
-            //{
-            //    for(int q = 0; q < c + 1; q++)
-            //    {
-            //        Console.Write(string.Join(' ', Array[p, q]));
-            //    }
-            //}
-
-
-
         }
     }
 }
